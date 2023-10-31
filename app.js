e4f1ba8d259ea145b5b9bf14edd8d37cd3dc33f9
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-10-30 20:21:55 -0400

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 
+const db = require('./data/database')
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -8,6 +9,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static('public'));
+
 app.use(authRoutes);
 
-app.listen(3000);
+
+
+
+db.connectToDatabase().then(function(){
+    app.listen(3000);
+}).catch(function(error){
+    console.log('failed to connect to the database!');
+    console.log(error);
+});
+
+
