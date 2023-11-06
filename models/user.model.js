ee6d88e3bce58a3442e10a192b9720f66f745475
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-11-06 14:26:58 -0500

    sixth commit

--- models/user.model.js
+++ models/user.model.js
@@ -17,6 +17,14 @@ class User {
     getUserwithSameEmail(){
         return db.getDb().collection('users').findOne({email: this.email});
     }
+
+    async existsAlready(){
+       const existingUser = await this.getUserwithSameEmail();
+       if (existingUser){
+        return true;
+       }
+       return false;
+    }
     async signup(){
         const hashedPassword = await bcrypt.hash(this.password, 12);
         await db.getDb().collection('users').insertOne({
